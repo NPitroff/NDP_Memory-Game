713fecbcd797b817d009c8053952b18bdb62f181
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-17 19:26:33 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div className="App border my-5">Place your code here</div>;
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,55 @@
+import AppleCard from "./images/AppleCard.jpg";
+import CareemCard from "./images/CareemCard.jpg";
+import CiscoCard from "./images/CiscoCard.jpg";
+import EbayCard from "./images/EbayCard.jpg";
+import FacebookCard from "./images/FacebookCard.jpg";
+import GoogleCard from "./images/GoogleCard.jpg";
+import IntelCard from "./images/IntelCard.jpg";
+import PaypalCard from "./images/PaypalCard.jpg";
+import TwitterCard from "./images/TwitterCard.jpg";
+import UberCard from "./images/UberCard.jpg";
+
+const cards = [
+  {
+    id: 1,
+    front: AppleCard
+  },
+  {
+    id: 2,
+    front: CareemCard
+  },
+  {
+    id: 3,
+    front: CiscoCard
+  },
+  {
+    id: 4,
+    front: EbayCard
+  },
+  {
+    id: 5,
+    front: FacebookCard
+  },
+  {
+    id: 6,
+    front: GoogleCard
+  },
+  {
+    id: 7,
+    front: IntelCard
+  },
+  {
+    id: 8,
+    front: PaypalCard
+  },
+  {
+    id: 9,
+    front: TwitterCard
+  },
+  {
+    id: 10,
+    front: UberCard
+  }
+];
+
+export default cards;
