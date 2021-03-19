262adba3ce12a4571fac7e390b3f4e2159bec851
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-18 19:06:54 -0700

    Created a page that loads the back of a car

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./App.css";
 
+//Card Data
+import cards from "./data";
+
+// Back of card
+import cardBack from "./images/CardBack.jpg";
+
 function App() {
-  return <div className="App border my-5">Place your code here</div>;
+  return (
+  <div className="App border my-5">
+    <div className="container">
+      <div className="row">
+        {/* {1} */}
+        <div className="col-3 my-1">
+          {/* {2} */}
+          <img alt="Back of a card"
+            className="mx-auto"
+            src={cardBack}
+            height="100%"
+            width="100%"
+            key="back"
+            />
+            {/* 3 */}
+        </div>
+      </div>
+    </div>
+  </div>
+  );
 }
 
 export default App;
