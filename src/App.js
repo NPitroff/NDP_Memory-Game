e948ad59a7789df6e9ccf4ab9d35c04f04c064f1
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-19 22:23:15 -0700

    integrated the card images to the app.js

--- src/App.js
+++ src/App.js
@@ -7,27 +7,30 @@ import cards from "./data";
 // Back of card
 import cardBack from "./images/CardBack.jpg";
 
+//Components
+import Card from "./Components/Card";
+
 function App() {
-  let cardsGrid = cards.map((card) => {
-    return(
-      <div className="col-3 my-1">
-        <img
-        alt="back of card"
-          className="mx-auto"
-          src={cardBack}
-          height="100%"
-          width="100%"
-          key="back"
-          />
-      </div>
-    );
-  })
+  let cardsGrid = cards.map(card  => (
+    <Card key={card.id} card={card} />
+    // return(
+    //   <div className="col-3 my-1">
+    //     <img
+    //     alt="back of card"
+    //       className="mx-auto"
+    //       src={cardBack}
+    //       height="100%"
+    //       width="100%"
+    //       key="back"
+    //       />
+    //   </div>
+  )
+  )
     return (
       <div className="App border my-5">
         <div className="container">
           <div className="row">
             {cardsGrid}
-              {/* 3 */}
             </div>
           </div>
         </div>
