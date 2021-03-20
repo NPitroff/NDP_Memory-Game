67e125e0055eb3c5c306f1eee8dd0b3287285f23
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-20 10:52:23 -0700

    Added the ReactCardFlip component to the Card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,22 +1,40 @@
 import React, { useState } from "react";
+// Card flipping component downlaoded with Yarn
+import ReactCardFlip from "react-card-flip";
 
 // Back of card
 import cardBack from "../images/CardBack.jpg";
 
+// useState hook to "flip" the cards
 const Card = ({ card, checkFlipped }) => {
     const [flipped, changeFlip] = useState(false);
+//function to flip the card
+    const handleFlip =() => {
+        changeFlip(!flipped);
+    };
 
     return (
         <div className="col-3 my-1">
+            <ReactCardFlip isFlipped ={flipped} flipDirection="horizontal">
             <img 
-                alt="The subject of the card"
+                alt="The back of the card"
                 className="mx-auto"
-                src={flipped ? card.front : cardBack}
+                src={cardBack}
+                height="100%"
+                width="100%"
+                key="back"
+                onClick={() => handleFlip()}
+                />
+                <img
+                alt="The front of the card"
+                className="mx-auto"
+                src={card.front}
                 height="100%"
                 width="100%"
                 key="front"
-                onClick={() => changeFlip(!flipped)}
+                onClick={() => handleFlip()}
                 />
+                </ReactCardFlip>
         </div>
     );
 };
