a0778808fa1514f0b5118a39fd62cb2c1c4fc8c5
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-20 10:39:08 -0700

    Added a useState hook to the Card component to flip the cards

--- src/App.js
+++ src/App.js
@@ -4,23 +4,8 @@ import "./App.css";
 //Card Data
 import cards from "./data";
 
-// Back of card
-import cardBack from "./images/CardBack.jpg";
-
 //Components
 import Card from "./Components/Card";
-
-// useState hook to count card clicks
-const Counter = () => {
-  const [counter, setCounter] = useState(0);
-  
-  return (
-    <div>
-      <h1>You have clicked this button {counter} times </h1>
-      <button onClick={() => setCounter(counter + 1)}>CLICK ME</button>
-    </div>
-  );
-};
 
 function App() {
   let cardsGrid = cards.map(card  => (
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Card = ({ card }) => {
+// Back of card
+import cardBack from "../images/CardBack.jpg";
+
+const Card = ({ card, checkFlipped }) => {
+    const [flipped, changeFlip] = useState(false);
+
     return (
         <div className="col-3 my-1">
             <img 
                 alt="The subject of the card"
                 className="mx-auto"
-                src={card.front}
+                src={flipped ? card.front : cardBack}
                 height="100%"
                 width="100%"
                 key="front"
+                onClick={() => changeFlip(!flipped)}
                 />
         </div>
     );
