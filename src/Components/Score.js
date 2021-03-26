34e79859713d953354d2968e0a1c028dcf778e01
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-03-26 09:28:25 -0700

    Added a score component that tracks the score or failed attempts depending on game mode

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Score extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
-}
+const Score = ({ mode, score, failedFlips, playerTurn }) => {
+  return (
+    <div>
+      (
+          {/* Checks for the mode and displays either the scores if multiplayer or failed attempts if solo */}
+      {mode === "multi" ? (
+        <div className="col-3 border">
+          <h3>Score</h3>
+          <table className="table table-responsive mx-auto">
+            <thead>
+              <tr>
+                <th scope="col" className={playerTurn && "text-danger"}>
+                  Player 1
+                </th>
+                <th scope="col" className={!playerTurn && "text-danger"}>
+                  Player 2
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{score[0]}</td>
+                <td>{score[1]}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="col-3 mt-3">
+          <div>
+            <h6>Failed Attempts: {failedFlips} </h6>
+          </div>
+        </div>
+      )}
+      )
+    </div>
+  );
+};
 
 export default Score;
